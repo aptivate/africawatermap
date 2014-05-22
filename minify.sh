c9ff5297868b94309a2301e4c527ebe34c1e52d3
Hamish Downer <hamish@aptivate.org>
2014-05-22 18:53:08 +0100

    add minify script

---
+++ minify.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# cd to the current directory
+cd "$(dirname "$0")"
+
+# minify js
+cat d3js/js/wwmap.js | uglifyjs > d3js/js/wwmap.min.js
+
+# minify mson
+jq --compact-output --ascii-output . d3js/data/lang_en.json > d3js/data/lang_en.min.json
+jq --compact-output --ascii-output . d3js/data/lang_fr.json > d3js/data/lang_fr.min.json
+
+# minify CSS
+minify d3js/css/wwmap.css
+
+git add */*/*.min.*
+git commit -m 'updated minified files'
